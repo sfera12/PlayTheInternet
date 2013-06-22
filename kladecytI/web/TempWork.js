047d4df19e0b8ec11a40b7a971c0c5a00d7806d6
Qyuz555@gmail.com <Qyuz555@gmail.com@b2ecd63b-ce3e-1a2e-dd73-34ad02cb03ca>
2013-06-22 15:24:32 +0000

    //fixed youtube js api eventHandler now working //inProgress select videos matching the criteria
    git-svn-id: https://kladecinfo.googlecode.com/svn/trunk@275 b2ecd63b-ce3e-1a2e-dd73-34ad02cb03ca

--- kladecytI/web/TempWork.js
+++ kladecytI/web/TempWork.js
@@ -7,3 +7,10 @@ _.flatten(_.reduce(sortedData, function (memo, item) {
 }, new Array()))
 
 DPGlobal.parseDate($('#start').val(), DPGlobal.parseFormat('dd-mm-yyyy'))
+
+--address=192.168.1.99
+
+var matchTextRegExp = new RegExp("выборы", "gi")
+var matchText = function(node) { return $(node).text().match(matchTextRegExp) != null ? true : false }
+var selectMatched = function(node, matchText) { if(matchText(node)) { $(node).addClass('ui-selected') } }
+_.each(calendarPlaylist.playlist, function(item) {  })
