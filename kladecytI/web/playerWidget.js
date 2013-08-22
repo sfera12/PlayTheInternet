b4d8da7493cf9048b3b1bc81bece8a9972173137
Qyuz555@gmail.com <Qyuz555@gmail.com@b2ecd63b-ce3e-1a2e-dd73-34ad02cb03ca>
2013-08-22 14:53:19 +0000

    //inProgress create PlayerHandler with widget
    //TODO save playerState in Player, not in Playlist
    //TODO fix parse.html playlist style width & overflow issue
    //TODO separate markups for each extension page
    //TODO optimize script loading for extension
    //TODO create loading bar
    //TODO https://www.youtube.com/embed/?listType=playlist&amp;list=PLhBgTdAWkxeBX09BokINT1ICC5IZ4C0ju&amp;showinfo=1
    //TODO syndicate issue
    //TODO strange vimeo player, check for seekTo and other methods
    
    git-svn-id: https://kladecinfo.googlecode.com/svn/trunk@306 b2ecd63b-ce3e-1a2e-dd73-34ad02cb03ca

---
+++ kladecytI/web/playerWidget.js
@@ -0,0 +1,64 @@
+function PlayerWidget(elementExpression) {
+    var self = this
+    this.jPlayerWidget = $('<div class="playerWidget"><div class="prev button"></div><div class="play button"></div><div class="next button"></div><a class="progressBarContainer" title="yoyoyoyo"><div class="progressBar"></div></a></div>').appendTo(elementExpression)
+
+    this.trackLength = 60
+    this.progress = 0
+    this.jProgressBar = this.jPlayerWidget.find('.progressBar')
+    this.jProgressBarContainer = $(this.jPlayerWidget.find('.progressBarContainer'))
+    this.jPlay = this.jPlayerWidget.find('.play')
+    this.jPrev = this.jPlayerWidget.find('.prev')
+    this.jNext = this.jPlayerWidget.find('.next')
+    PlayerWidget.prototype.progressBar = function (progress) {
+        if (typeof progress == "undefined") {
+            return this.progress
+        }
+        this.progress = progress
+        this.jProgressBar.css('width', this.progress + '%')
+    }
+
+    this.jPlayerWidget.on('click', '.play', function () {
+        var jElement = $(this)
+        jElement.removeClass('play')
+        jElement.addClass('pause')
+    })
+
+    this.jPlayerWidget.on('click', '.pause', function () {
+        var jElement = $(this)
+        jElement.removeClass('pause')
+        jElement.addClass('play')
+    })
+    this.jPlayerWidget.on('click', '.prev', function () {
+    })
+    this.jPlayerWidget.on('click', '.next', function () {
+    })
+    this.jPlayerWidget.on('click', '.progressBarContainer', function(evt) {
+        var jElement = $(this)
+        var moveTo = evt.offsetX / jElement.width() * 100
+        self.progressBar(moveTo)
+    })
+
+    this.jPlayerWidget.on('mousedown', '.button', function() {
+        var jElement = $(this)
+        jElement.addClass('mouseDown')
+    })
+    this.jPlayerWidget.on('mouseup', '.button', function() {
+        var jElement = $(this)
+        jElement.removeClass('mouseDown')
+    })
+
+    var throttleMouseMove = _.throttle(function(jElement, evt) {
+        var progress = evt.offsetX / jElement.width() * 100
+        var seconds = self.trackLength * ( progress / 100)
+        self.jProgressBarContainer.tooltip("option", "content", convert(seconds))
+    }, 100
+//        , {trailing: false}
+    )
+
+    this.jPlayerWidget.on('mousemove', '.progressBarContainer', function(evt) {
+        throttleMouseMove($(this), evt)
+    })
+    self.jProgressBarContainer.tooltip({track: true})
+
+
+}
