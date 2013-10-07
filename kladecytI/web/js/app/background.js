517d3f734a996d7af9d65aad20e5298ec11cd90e
Qyuz555@gmail.com <Qyuz555@gmail.com@b2ecd63b-ce3e-1a2e-dd73-34ad02cb03ca>
2013-10-07 10:09:12 +0000

    //done background is require app
    //TODO remove temporary while working on PTI pieces
    //TODO fix parse.html playlist style width & overflow issue
    //TODO separate markups for each extension page
    //TODO optimize script loading for extension
    //TODO create loading bar
    //TODO https://www.youtube.com/embed/?listType=playlist&amp;list=PLhBgTdAWkxeBX09BokINT1ICC5IZ4C0ju&amp;showinfo=1
    //TODO syndicate issue
    //TODO strange vimeo player, check for seekTo and other methods
    //TODO bootstrap.js conflicts with jQuery tooltip
    //TODO implement pti errorTimeout, pti playTimeout(loadingTimeout)
    //TODO video switch and resuming fails when switched from main popup player to background player( https://www.youtube.com/watch?v=yxMcuzOgVrU )
    
    git-svn-id: https://kladecinfo.googlecode.com/svn/trunk@354 b2ecd63b-ce3e-1a2e-dd73-34ad02cb03ca

--- kladecytI/web/js/app/background.js
+++ kladecytI/web/js/app/background.js
@@ -1,4 +1,4 @@
-define(["playlist", "iframe-observer"], function(Playlist, pti) {
+define(["playlist", "iframe-observer"], function(a, pti) {
     $(document).ready(function () {
         window.windowId = 'backgroundPageId'
         window.playlist = new Playlist("#ulSecond",
