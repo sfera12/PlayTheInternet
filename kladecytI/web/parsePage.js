bdce13917aa3cce57fe8a17e1230c836a7408971
Qyuz555@gmail.com <Qyuz555@gmail.com@b2ecd63b-ce3e-1a2e-dd73-34ad02cb03ca>
2013-10-14 09:38:02 +0000

    //inProgress code cleanup - remove unsued files
    //TODO remove temporary while working on PTI pieces
    //TODO fix parse.html playlist style width & overflow issue
    //TODO separate markups for each extension page
    //TODO optimize script loading for extension
    //TODO create loading bar
    //TODO https://www.youtube.com/embed/?listType=playlist&amp;list=PLhBgTdAWkxeBX09BokINT1ICC5IZ4C0ju&amp;showinfo=1
    //TODO syndicate issue
    //TODO strange vimeo player, check for seekTo and other methods
    //TODO bootstrap.js conflicts with jQuery tooltip
    //TODO implement pti errorTimeout, pti playTimeout(loadingTimeout)
    //TODO video switch and resuming fails when switched from main popup player to background player( https://www.youtube.com/watch?v=yxMcuzOgVrU )
    
    git-svn-id: https://kladecinfo.googlecode.com/svn/trunk@363 b2ecd63b-ce3e-1a2e-dd73-34ad02cb03ca

--- kladecytI/web/parsePage.js
+++
@@ -1,5 +0,0 @@
-if (typeof playTheInternetParse === "undefined") {
-    alert('no playTheInternetParse function, reload this tab');
-} else {
-    chrome.runtime.sendMessage({operation:"parsedPlaylist", data:playTheInternetParse(document.documentElement.innerHTML)});
-}
--- kladecytI/web/playFirstLoaded.js
+++
@@ -1,9 +0,0 @@
-//TODO 2013-08-21 DO SOMETHING WITH PLAYFIRSTLOADED and onceloaded
-var playFirstLoaded = _.after(3, function () {
-    console.log('playFirstLoaded')
-    onceLoaded()
-})
-if (chrome.extension) {
-    var onceLoaded = function () {
-    }
-}
