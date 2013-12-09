641bdd940d35dcdc0aa7337bf1897320bc1c7f73
Qyuz555@gmail.com <Qyuz555@gmail.com@b2ecd63b-ce3e-1a2e-dd73-34ad02cb03ca>
2013-12-09 13:20:51 +0000

    0.63 //done nothing found while parsing page
    //TODO one click add from ParsedPlaylist
    //TODO remove temporary while working on PTI pieces
    //TODO fix parse.html playlist style width & overflow issue
    //TODO separate markups for each extension page
    //TODO optimize script loading for extension
    //TODO create loading bar
    //TODO https://www.youtube.com/embed/?listType=playlist&amp;list=PLhBgTdAWkxeBX09BokINT1ICC5IZ4C0ju&amp;showinfo=1
    //TODO syndicate issue
    //TODO strange vimeo player, check for seekTo and other methods
    //TODO bootstrap.js conflicts with jQuery tooltip
    //TODO implement pti errorTimeout, pti playTimeout(loadingTimeout)
    //TODO video switch and resuming fails when switched from main popup player to background player( https://www.youtube.com/watch?v=yxMcuzOgVrU )
    
    git-svn-id: https://kladecinfo.googlecode.com/svn/trunk@476 b2ecd63b-ce3e-1a2e-dd73-34ad02cb03ca

--- kladecytI/web/js/app/popup/parse-content.js
+++ kladecytI/web/js/app/popup/parse-content.js
@@ -6,28 +6,27 @@ define(["playlist", "app/chrome/extension"], function(a, extension) {
 //                        "from the extension");
             if (request.greeting == "hello")
                 sendResponse({farewell:"goodbye"});
-            chrome.storage.local.get(['parseHeaderOptions'], function (options) {
-                options = extension.prepareOptions(options, { size: 'list', split: 'one'})
-                window.parsedPlaylist = new Playlist('#parsedPlaylist', {
-                        elementSize: options.size,
-                        elementSplit: options.split,
-                        headerClick: extension.headerClick.bind({parseHeaderOptions: {}}),
-                        execute: [
-                            Playlist.prototype.addAction
-                        ]
-                    }
-                );
-                parsedPlaylist.playlistEmpty();
-                parsedPlaylist.addSongsToPlaylist(parsedPlaylist.parseSongIds(request.data))
-            })
             if (request.operation == "parsedPlaylist") {
-                request.data == '' && $('#parsedDiv').append(PTITemplates.prototype.ParsePlayTheInternetParseNothingFound(request))
+                request.data != '' ? parsedPlaylist.addSongsToPlaylist(parsedPlaylist.parseSongIds(request.data)) : $('#parsedDiv').append(PTITemplates.prototype.ParsePlayTheInternetParseNothingFound(request))
             } else if(request.operation == "parsePlayTheInternetParseFunctionMissing") {
                 $('#parsedDiv').append(PTITemplates.prototype.ParsePlayTheInternetParseFunctionMissing(request))
             }
         }
     );
-    chrome.tabs.executeScript(null, {file: "/js/app/popup/parsePage.js"}, function (parse) {
-        _.isUndefined(parse) && $('#parsedDiv').append(PTITemplates.prototype.ParsePlayTheInternetParseNothingFound({href: window.location.href}))
-    });
+    chrome.storage.local.get(['parseHeaderOptions'], function (options) {
+        options = extension.prepareOptions(options, { size: 'list', split: 'one'})
+        window.parsedPlaylist = new Playlist('#parsedPlaylist', {
+                elementSize: options.size,
+                elementSplit: options.split,
+                headerClick: extension.headerClick.bind({parseHeaderOptions: {}}),
+                execute: [
+                    Playlist.prototype.addAction
+                ]
+            }
+        );
+        parsedPlaylist.playlistEmpty();
+        chrome.tabs.executeScript(null, {file: "/js/app/popup/parsePage.js"}, function (parse) {
+            _.isUndefined(parse) && $('#parsedDiv').append(PTITemplates.prototype.ParsePlayTheInternetParseNothingFound({href: window.location.href}))
+        });
+    })
 })
