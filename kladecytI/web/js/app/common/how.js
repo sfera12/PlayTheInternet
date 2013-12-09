0e490bf05e5157af261c36afe00f643974c28f3c
qyuz555@gmail.com <qyuz555@gmail.com@b2ecd63b-ce3e-1a2e-dd73-34ad02cb03ca>
2013-12-09 22:26:50 +0000

    0.63 //done how tab with updates highlight
    git-svn-id: https://kladecinfo.googlecode.com/svn/trunk@477 b2ecd63b-ce3e-1a2e-dd73-34ad02cb03ca

--- kladecytI/web/js/app/common/how.js
+++ kladecytI/web/js/app/common/how.js
@@ -14,14 +14,14 @@ define(["playlist"], function () {
     Playlist.prototype.setSlimScroll('#howDivContainer', '100%')
     $('#howDivContainer').on('click', '.temp-panel-heading-how', function () {
 //        console.log(this)
-        var $how = $(this), $content = $how.siblings()
-        if ($content.length == 0) {
+        var $how = $(this), $contentSiblings = $how.siblings()
+        if ($contentSiblings.length == 0) {
             var videoId = $how.attr('pti-video'), $el = $('<div class="temp-how-item"> <iframe width="560" height="315" src="http://www.youtube.com/embed/' + videoId + '" frameborder="0" allowfullscreen></iframe> </div>').hide()
             $how.after($el)
             $el.show(150)
         } else {
-            $content.hide(400, function () {
-                $content.remove()
+            $contentSiblings.hide(400, function () {
+                $contentSiblings.remove()
             })
         }
     })
