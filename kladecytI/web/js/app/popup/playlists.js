3a17b067f6ddf9460c33a4d71e50ed8874e6a18d
Qyuz555@gmail.com <Qyuz555@gmail.com@b2ecd63b-ce3e-1a2e-dd73-34ad02cb03ca>
2013-12-19 18:18:42 +0000

    /g 0.631 /l playlist "scrollTo .selected .pti-element-song upon initialization in chrome popup"
    git-svn-id: https://kladecinfo.googlecode.com/svn/trunk@502 b2ecd63b-ce3e-1a2e-dd73-34ad02cb03ca

--- kladecytI/web/js/app/popup/playlists.js
+++ kladecytI/web/js/app/popup/playlists.js
@@ -17,7 +17,7 @@ define(["app/chrome/extension", "app/common/hash-qr", "playlist"], function(exte
         var index = playlist.jPlaylist.find('.selected').index()
         var songsCount = playlist.jPlaylist.find('.pti-element-song').length
         var scrollHeight = playlist.jPlaylist.prop('scrollHeight')
-        var scrollTo = scrollHeight / songsCount * index
+        var scrollTo = scrollHeight / songsCount * index - playlist.jPlaylist.height() / 2
         playlist.jPlaylist.slimscroll({scrollTo: scrollTo + 'px'})
     })
 
