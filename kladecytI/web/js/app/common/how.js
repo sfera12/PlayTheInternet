3fa1515437e1d10bdf081983857e0580b3b36824
Qyuz555@gmail.com <Qyuz555@gmail.com@b2ecd63b-ce3e-1a2e-dd73-34ad02cb03ca>
2013-12-21 15:59:33 +0000

    /g 0.632 /l how "hover and selected styles"
    git-svn-id: https://kladecinfo.googlecode.com/svn/trunk@510 b2ecd63b-ce3e-1a2e-dd73-34ad02cb03ca

--- kladecytI/web/js/app/common/how.js
+++ kladecytI/web/js/app/common/how.js
@@ -16,10 +16,12 @@ define(["playlist"], function () {
 //        console.log(this)
         var $how = $(this), $contentSiblings = $how.siblings()
         if ($contentSiblings.length == 0) {
+            $how.addClass('selected')
             var videoId = $how.attr('pti-video'), $el = $('<div class="temp-how-item"> <iframe width="560" height="315" src="http://www.youtube.com/embed/' + videoId + '" frameborder="0" allowfullscreen></iframe> </div>').hide()
             $how.after($el)
             $el.show(150)
         } else {
+            $how.removeClass('selected')
             $contentSiblings.hide(400, function () {
                 $contentSiblings.remove()
             })
