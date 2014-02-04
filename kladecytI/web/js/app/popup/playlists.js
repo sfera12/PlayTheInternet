8fcc02d86f62f5a5bf090c7bfd95271a26613a77
Qyuz555@gmail.com <Qyuz555@gmail.com@b2ecd63b-ce3e-1a2e-dd73-34ad02cb03ca>
2014-02-04 12:57:46 +0000

    /g 0.633
    //done headerClick for playlist, playlists
    //inProgress garbageCollector introduced
    
    //TODO TextAreaParse
    
    //TODO Playlist selected_ listener
    //TODO Playlists click handlers
    //TODO add console.log to filtered by redrawListen, by source===uid and when redrawn
    //TODO migrate localStorage to 0.633
    //TODO ? tab missing margin-bottom 1px
    //TODO right tabs hides left tabs player
    
    //TODO https://www.youtube.com/embed/?listType=playlist&amp;list=PLhBgTdAWkxeBX09BokINT1ICC5IZ4C0ju&amp;showinfo=1
    //TODO syndicate issue
    //TODO strange vimeo player, check for seekTo and other methods
    //TODO implement pti errorTimeout, pti playTimeout(loadingTimeout)
    //TODO video switch and resuming fails when switched from main popup player to background player( https://www.youtube.com/watch?v=yxMcuzOgVrU )
    
    git-svn-id: https://kladecinfo.googlecode.com/svn/trunk@556 b2ecd63b-ce3e-1a2e-dd73-34ad02cb03ca

--- kladecytI/web/js/app/popup/playlists.js
+++ kladecytI/web/js/app/popup/playlists.js
@@ -1,42 +1,33 @@
-define(["app/common/hash-qr", "pti-playlist"], function(redrawHashAndQRCode, Playlist) {
-    chrome.storage.local.get(['playlistHeaderOptions'], function (options) {
-        var selected = $.jStorage.get("selected_backgroundPageId"), index = selected && selected.index >= 0 && selected.index
-        window.playlist = new Playlist("#ulSecond", {
-                id: chrome.extension.getBackgroundPage().windowId,
-                scrollTo: index,
-                recalculateJContentImmediateCallback: redrawHashAndQRCode,
-                elementSize: options.size,
-                elementSplit: options.split,
-                connectWith: "connected-playlist",
-                headerConfigKey: "lConfigPlaylistHeader",
-                execute: [
-                    Playlist.prototype.playAction
-                ]
-            }
-        );
-    })
+define(["app/common/hash-qr", "pti-playlist"], function (redrawHashAndQRCode, Playlist) {
+    var selected = $.jStorage.get("selected_backgroundPageId"), index = selected && selected.index >= 0 && selected.index
+    window.playlist = new Playlist("#ulSecond", {
+            id: chrome.extension.getBackgroundPage().windowId,
+            scrollTo: index,
+            recalculateJContentImmediateCallback: redrawHashAndQRCode,
+            connectWith: "connected-playlist",
+            headerConfigKey: "lConfigPlaylistHeader",
+            execute: [
+                Playlist.prototype.playAction
+            ]
+        }
+    );
 
-    chrome.storage.local.get(['textAreaParseHeaderOptions'], function(options) {
-        var createPlaylist = _.once(function() {
-            window.textAreaParsePlaylist = new Playlist("#textAreaParsePlaylist", {
-                    playerType: false,
-                    elementSize: options.size,
-                    elementSplit: options.split,
-                    connectWith: "connected-playlist",
-                    headerConfigKey: "lConfigTextAreaParsePlaylistHeader",
+    var createPlaylist = _.once(function () {
+        window.textAreaParsePlaylist = new Playlist("#textAreaParsePlaylist", {
+            playerType: false,
+            connectWith: "connected-playlist",
+            headerConfigKey: "lConfigTextAreaParsePlaylistHeader",
 //                    execute: [
 //                        Playlist.prototype.addAction
 //                    ]
-                }
-            );
         })
-        $('#tAreaParseButton').click(function () {
-            var tAreaText = $('#tArea').val()
-            createPlaylist()
-            textAreaParsePlaylist.playlistEmpty();
-            require(['cparse'], function() {
+    })
+    $('#tAreaParseButton').click(function () {
+        var tAreaText = $('#tArea').val()
+        createPlaylist()
+        textAreaParsePlaylist.playlistEmpty();
+        require(['cparse'], function () {
                 textAreaParsePlaylist.addSongsToPlaylist(textAreaParsePlaylist.parseSongIds(playTheInternetParse(tAreaText)), true)
-            })
         })
     })
 })
